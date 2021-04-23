be6f4658a96612152ad9d525a2c2c2adc9bb8909
Valentin Condamy <valentin.condamy.2020@gmail.com>
2021-04-23 22:39:36 +0200

    add_panier.js

--- js/product.js
+++ js/product.js
@@ -48,64 +48,15 @@ async function displayCamerasDetails() {
         <p>${cameraDetail.description}</p>
         <p>${cameraDetail.price / 100 + " €"}</p>
 
-        <label for="option">Lentille(s):</label>
-        <select name="option" id="chosenLense">
+        <label for="Lense">Lentille(s):</label>
+        <select name="Lense" id="chosenLense">
             <option disabled value=""lentille</option>
         </select>
         </div>`
     cameraDetail.lenses.forEach(camera => {
-        let chosenOption = document.createElement("Option");
+        let chosenOption = document.createElement("option");
         document
             .getElementById("chosenLense")
             .appendChild(chosenOption).innerHTML = camera;
     });
-}
-
-
-async function displayCamera(camera) {
-
-    const listCamera = document.getElementById('list-camera');
-
-    let cameraContentbox = document.createElement("div");
-    let cameraContent = document.createElement("article");
-    let cameratext = document.createElement("div");
-    let cameraPicturebox = document.createElement("div");
-    let cameraPicture = document.createElement("img");
-    let cameraName = document.createElement("h2")
-    let cameraPrice = document.createElement("p")
-    let cameralinkBox = document.createElement("div");
-    let cameralink = document.createElement("a")
-
-    //nestings:
-
-    listCamera.appendChild(cameraContentbox);
-    cameraContentbox.appendChild(cameraContent);
-    cameraContent.appendChild(cameraPicturebox);
-    cameraPicturebox.appendChild(cameraPicture);
-    cameraContent.appendChild(cameratext);
-    cameratext.appendChild(cameraName);
-    cameratext.appendChild(cameraPrice);
-    cameratext.appendChild(cameralinkBox);
-    cameralinkBox.appendChild(cameralink)
-
-    // HTML tags:
-
-    cameraContentbox.setAttribute("class", "camera_contentbox ");
-    cameraContent.setAttribute("class", "camera_content");
-    cameratext.setAttribute("class", "camera_text");
-    cameraPicturebox.setAttribute("class", "camera_Picturebox");
-    cameraPicture.setAttribute("alt", "Photo de la caméra");
-    cameraName.setAttribute("class", "camera_name");
-    cameraPrice.setAttribute("class", "camera_Price");
-    cameralinkBox.setAttribute("class", "camera_linkbox");
-    cameralink.setAttribute("href", "product.html?id=" + camera._id);
-    cameralink.setAttribute("class", "camera_link")
-
-
-    // content of tags:
-    cameraPicture.src = camera.imageUrl;
-    cameraName.textContent = camera.name;
-    cameraPrice.textContent = camera.price / 100 + " €";
-    cameralink.textContent = "voir la fiche produit";
-
 }
