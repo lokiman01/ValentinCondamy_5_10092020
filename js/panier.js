8c64327350b653499158a13ece67f212e1499783
Valentin Condamy <valentin.condamy.2020@gmail.com>
2021-05-17 13:26:38 +0200

    addTotal

--- js/panier.js
+++ js/panier.js
@@ -7,67 +7,74 @@ function start() {
     let panier = localStorage.getItem("panier");
     // Réforme de l'objet avec .parse
     let panierArray = JSON.parse(panier);
-    console.log(panierArray);
     // lien HTML
-    var body = document.getElementsByTagName("body")[0];
+    let body = document.getElementsByTagName("body")[0];
     // création d'un tableau "<table>" 
-    var tbl = document.createElement("table");
+    let tbl = document.createElement("table");
     // création d'un <tbody> afin de regrouper les éléments <tr> afin de forme le corps du t 
-    var tblBody = document.createElement("tbody")
+    let tblBody = document.createElement("tbody")
     // création de la boucle index
+    let total = 0 
+    
     for (let index = 0; index < panierArray.length; index++) {
         // incrémentation
         const element = panierArray[index];
         // création d'une ligne "row"
-        var row = document.createElement("tr");
-  
-      
-        // Création de l'element <td> 
-        var cellName = document.createElement("td");
-        // insertion du texte depuis le tableau
-        var cellNameTexte = document.createTextNode(element.name);
-        // Création de l'element <td> 
-        var cellLenses = document.createElement("td");
-        // insertion du texte depuis le tableau
-        var cellLensesTexte = document.createTextNode(element.lenses);
-        // Création de l'element <td>   
-        var cellPrice = document.createElement("td");
-        // insertion du texte depuis le tableau
-        var cellPriceTexte = document.createTextNode(element.price / 100 + " €");
-        // Création de l'element <td> 
-        var cellLenses = document.createElement("td");
-        // insertion du texte depuis le tableau
-        var cellLensesTexte = document.createTextNode(element.lenses);
-        // Création de l'element <td> 
-        var cellQuantity = document.createElement("td");
-        // insertion du texte depuis le tableau
-        var cellQuantityTexte = document.createTextNode(element.quantity);
-        // Création de l'element <td> 
-        var cellTotal = document.createElement("td");
-        // insertion du texte depuis le tableau
-        var cellTotalTexte = document.createTextNode(element.quantity * element.price / 100 + " €" );
+        let row = document.createElement("tr");
+
+        let cellName = document.createElement("td");
+        let cellNameTexte = document.createTextNode(element.name);
+
+        let cellPrice = document.createElement("td");
+        let cellPriceTexte = document.createTextNode(element.price / 100 + " €");
+    
+        let cellLenses = document.createElement("td"); 
+        let cellLensesTexte = document.createTextNode(element.lenses);
+    
+        let cellQuantity = document.createElement("td");
+        let cellQuantityTexte = document.createTextNode(element.quantity);
+    
+        let cellSousTotal = document.createElement("td");
+        let cellSousTotalTexte = document.createTextNode(element.quantity * element.price / 100 + " €");
+
+        let cellButton = document.createElement("BUTTON");
+        cellButton.innerHTML = '<button >X</button>';
+
+        let pocket = element.quantity * (element.price / 100);
+        total += pocket;
 
         cellName.appendChild(cellNameTexte);
         cellLenses.appendChild(cellLensesTexte);
         cellPrice.appendChild(cellPriceTexte);
         cellQuantity.appendChild(cellQuantityTexte);
-        cellTotal.appendChild(cellTotalTexte);
+        cellSousTotal.appendChild(cellSousTotalTexte);
 
         row.appendChild(cellName);
         row.appendChild(cellLenses);
         row.appendChild(cellPrice);
         row.appendChild(cellQuantity);
-        row.appendChild(cellTotal);
-   
+        row.appendChild(cellSousTotal);
+        row.appendChild(cellButton);
+
         tblBody.appendChild(row);
     }
-  
+
+    let totalRow = document.createElement("tr");
+
+    let cellTotal = document.createElement("td");
+    let cellTotalTexte = document.createTextNode("Total: "+total+ " €");
+    
+    cellTotal.appendChild(cellTotalTexte);
+    totalRow.appendChild(cellTotal)
+    tblBody.appendChild(totalRow);
+
+    console.log(total)
     //  <tbody> dans => <table>
     tbl.appendChild(tblBody);
     //  <table> dans => <body>
     body.appendChild(tbl);
     // un peu de css
-    tbl.setAttribute("border", "2");
+    tbl.setAttribute("border", "1");
     
-  }
+}
 
